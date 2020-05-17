7ff48122e9de7e9340b94fee25dee8e2bacb2541
James Sutton <james.sutton1@btinternet.com>
2020-05-17 21:14:58 +1000

    Drawing lines and added vecs

--- src/main.c
+++ src/main.c
@@ -1,23 +1,9 @@
 #include <windows.h>
-#include <stdint.h>
 
-#define false 0;
-#define true 1;
+#include "types.h"
+#include "maths.h"
 
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float f32;
-typedef double f64;
-typedef s32 b32;
-
-typedef struct Backbuffer
-{
+typedef struct Backbuffer {
 	s32 width;
 	s32 height;
 	void* memory;
@@ -27,7 +13,51 @@ typedef struct Backbuffer
 static Backbuffer backbuffer;
 static b32 running;
 
-LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+static void Swap(float* xp, float* yp)
+{
+	float temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+static void DrawPixel(struct Backbuffer* buffer, s32 x, s32 y, vec3 colour)
+{
+	if (x < 0 || y < 0 || x >= buffer->width || y >= buffer->height) 
+		return;
+
+	s32* pixel = (s32*)buffer->memory + ((s64)x + (s64)y * buffer->width);
+	*pixel = (((s32)colour.r << 16) | ((s32)colour.g << 8) | (s32)colour.b);
+}
+
+static void DrawLine(struct Backbuffer* buffer, vec2 v0, vec2 v1, vec3 colour)
+{
+	s32 dx = abs(v1.x - v0.x);
+	s32 dy = abs(v1.y - v0.y);
+
+	if (dx > dy) {
+		if (v0.x > v1.x) {
+			Swap(&v0.x, &v1.x);
+			Swap(&v0.y, &v1.y);
+		}
+		for (s32 x = v0.x; x <= v1.x; x++) {
+			f32 t = (f32)(x - v0.x) / (f32)(v1.x - v0.x);
+			s32 y = (s32)(v0.y + (v1.y - v0.y) * t);
+			DrawPixel(buffer, x, y, colour);
+		}
+	} else {
+		if (v0.y > v1.y) {
+			Swap(&v0.y, &v1.y);
+			Swap(&v0.x, &v1.x);
+		}
+		for (s32 y = v0.y; y <= v1.y; y++) {
+			f32 t = (f32)(y - v0.y) / (f32)(v1.y - v0.y);
+			s32 x = (s32)(v0.x + (v1.x - v0.x) * t);
+			DrawPixel(buffer, x, y, colour);
+		}
+	}
+}
+
+static LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT result = -1;
 	switch (message) {
@@ -65,7 +95,7 @@ LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lPa
 	return result;
 }
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+static int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	running = true;
 
@@ -106,12 +136,12 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		}
 
 		// Clear backbuffer
-		s32* pixel = (s32*)backbuffer.memory;
-		for (s32 y = 0; y < backbuffer.height; y++) {
-			for (s32 x = 0; x < backbuffer.width; x++) {
-				*pixel++ = 0x00FF00;
-			}
-		}
+		memset(backbuffer.memory, 0, (s64)backbuffer.width * (s64)backbuffer.height * sizeof(s32));
+
+		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(50, 30), Vec3f(0.f, 0.f, 255.f));
+		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(30, 50), Vec3f(0.f, 255.f, 0.f));
+		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(10, 30), Vec3f(255.f, 0.f, 0.f));
+		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(30, 10), Vec3f(255.f, 255.f, 255.f));
 		
 		StretchDIBits(device_context, 
 			0, 0, 
---
+++ src/maths.h
@@ -0,0 +1,106 @@
+#ifndef MATHS_H
+#define MATHS_H
+
+#include <math.h>
+
+#include "types.h"
+
+typedef struct vec2 {
+	struct {
+		f32 x, y;
+	};
+	struct {
+		f32 u, v;
+	};
+	f32 elements[2];
+} vec2;
+
+typedef union vec3 {
+	struct {
+		f32 x, y, z;
+	};
+	struct {
+		f32 u, v, w;
+	};
+	struct {
+		f32 r, g, b;
+	};
+	f32 elements[3];
+} vec3;
+
+typedef union vec4 {
+	struct {
+		union {
+			vec3 xyz;
+			struct {
+				f32 x, y, z;
+			};
+		};
+		f32 w;
+	};
+	struct {
+		union {
+			vec3 rgb;
+			struct {
+				f32 r, g, b;
+			};
+		};
+		f32 a;
+	};
+	f32 elements[4];
+} vec4;
+
+inline vec2 Vec2i(s32 x, s32 y)
+{
+	vec2 result = { 0 };
+	result.x = (f32)x;
+	result.y = (f32)y;
+	return result;
+}
+
+inline vec2 Vec2f(f32 x, f32 y)
+{
+	vec2 result = { 0 };
+	result.x = x;
+	result.y = y;
+	return result;
+}
+
+inline vec3 Vec3i(s32 x, s32 y, s32 z)
+{
+	vec3 result = { 0 };
+	result.x = (f32)x;
+	result.y = (f32)y;
+	result.z = (f32)z;
+	return result;
+}
+
+inline vec3 Vec3f(f32 x, f32 y, f32 z)
+{
+	vec3 result = { 0 };
+	result.x = x;
+	result.y = y;
+	result.z = z;
+	return result;
+}
+
+inline vec4 Vec4i(s32 x, s32 y, s32 z, s32 w)
+{
+	vec4 result = { 0 };
+	result.x = (f32)x;
+	result.y = (f32)y;
+	result.z = (f32)z;
+	result.w = (f32)w;
+	return result;
+}
+
+inline vec4 Vec4f(f32 x, f32 y, f32 z, f32 w)
+{
+	vec4 result = { 0 };
+	result.x = x;
+	result.y = y;
+	result.z = z;
+	result.w = w;
+	return result;
+}
+#endif 
