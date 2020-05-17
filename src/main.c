d0e59d8693e48684305631c870f289e03a227b4a
James Sutton <james.sutton1@btinternet.com>
2020-05-17 20:06:14 +1000

    Creating a Win32 window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,79 @@
-#include <stdio.h>
+#include <windows.h>
+#include <stdint.h>
 
-int main()
+#define false 0;
+#define true 1;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float f32;
+typedef double f64;
+typedef s32 b32;
+
+static b32 running;
+
+LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	printf("Hello World!");
+	LRESULT result = -1;
+	switch (message)
+	{
+		case WM_CLOSE:
+		case WM_DESTROY:
+		case WM_QUIT:
+		{
+			running = false;
+			PostQuitMessage(0);
+		} break;
+		default:
+			result = DefWindowProc(window, message, wParam, lParam);
+		}
+	return result;
+}
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	running = true;
+
+	WNDCLASS window_class = {0};
+
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpfnWndProc = WindowProc;
+	window_class.lpszClassName = L"Win32 Class";
+
+	if (!RegisterClass(&window_class)) 
+		return false;
+
+	HWND window = CreateWindow(
+		window_class.lpszClassName,
+		L"Renderer", 
+		WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT, 
+		CW_USEDEFAULT, CW_USEDEFAULT, 
+		0, 
+		0, 
+		hInstance, 
+		0
+	);
+
+	if (!window)
+		return false;
+
+	ShowWindow(window, SW_SHOW);
+	UpdateWindow(window);
+	
+	while (running) {
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+	}
+
+	return 0;
 }
