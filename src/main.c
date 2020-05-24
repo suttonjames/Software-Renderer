0134e24ac00e03455980a4843f4de03b4d6e709d
James Sutton <james.sutton1@btinternet.com>
2020-05-24 19:12:34 +1000

    Zbuffering

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@ typedef struct Backbuffer {
 
 static Backbuffer backbuffer;
 static b32 running;
+static f32 *zbuffer;
 
 static void Swap(f32* xp, f32* yp)
 {
@@ -86,7 +87,7 @@ static int InTriangle(vec2 a, vec2 b, vec2 c, vec2 p, f32* sp, f32* tp)
 	return (s >= 0 && t >= 0 && s + t <= 1);
 }
 
-static void FillTriangle(Backbuffer* buffer, vec2 point0, vec2 point1, vec2 point2, vec3 colour0, vec3 colour1, vec3 colour2)
+static void FillTriangle(Backbuffer* buffer, vec3 point0, vec3 point1, vec3 point2, vec3 colour0, vec3 colour1, vec3 colour2, f32 *zbuffer)
 {
 	s32 min_x = buffer->width - 1, min_y = buffer->height - 1;
 	s32 max_x = 0, max_y = 0;
@@ -105,12 +106,20 @@ static void FillTriangle(Backbuffer* buffer, vec2 point0, vec2 point1, vec2 poin
 		for (s32 i = min_x; i < max_x; i++) {
 			f32 s, t;
 			vec2 point = Vec2i(i, j);
-			if (InTriangle(point0, point1, point2, point, &s, &t)) {
+			vec2 point02 = Vec2i(point0.x, point0.y);
+			vec2 point12 = Vec2i(point1.x, point1.y);
+			vec2 point22 = Vec2i(point2.x, point2.y);
+			if (InTriangle(point02, point12, point22, point, &s, &t)) {
+				f32 z = (1.0f - s - t) * point0.z + s * point1.z + t * point2.z;
 				vec3 colour;
 				colour.r = ((1.0f - s - t) * colour0.r + s * colour1.r + t * colour2.r);
 				colour.g = ((1.0f - s - t) * colour0.g + s * colour1.g + t * colour2.g);
 				colour.b = ((1.0f - s - t) * colour0.b + s * colour1.b + t * colour2.b);
-				DrawPixel(buffer, point.x, point.y, colour);
+				if (zbuffer[j * buffer->width + i] < z) {
+					DrawPixel(buffer, point.x, point.y, colour);
+					zbuffer[j * buffer->width + i] = z;
+				}
+				
 			}
 		}
 	}
@@ -148,6 +157,12 @@ static LRESULT CALLBACK WindowProc(HWND window, UINT message, WPARAM wParam, LPA
 			backbuffer.bitmapInfo.bmiHeader.biPlanes = 1;
 			backbuffer.bitmapInfo.bmiHeader.biBitCount = 32;
 			backbuffer.bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+			if (zbuffer)
+				VirtualFree(zbuffer, 0, MEM_RELEASE);
+
+			zbuffer = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
 		} break;
 		default:
 			result = DefWindowProc(window, message, wParam, lParam);
@@ -200,20 +215,22 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 		// Clear backbuffer
 		memset(backbuffer.memory, 0, (s64)backbuffer.width * (s64)backbuffer.height * sizeof(s32));
+		// Clear zbuffer
+		memset(zbuffer, 0, (s64)backbuffer.width * (s64)backbuffer.height * sizeof(f32));
 
 		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(50, 30), Vec3f(0.f, 0.f, 255.f));
 		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(30, 50), Vec3f(0.f, 255.f, 0.f));
 		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(10, 30), Vec3f(255.f, 0.f, 0.f));
 		DrawLine(&backbuffer, Vec2i(30, 30), Vec2i(30, 10), Vec3f(255.f, 255.f, 255.f));
 
-		FillTriangle(&backbuffer, Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80), Vec3f(255.f, 0.f, 0.f), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 0.f, 255.f));
-		FillTriangle(&backbuffer, Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180), Vec3f(255.f, 255.f, 255.f), Vec3f(255.f, 255.f, 255.f), Vec3f(0.f, 0.f, 255.f));
-		FillTriangle(&backbuffer, Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 255.f, 0.f));
+		FillTriangle(&backbuffer, Vec3i(10, 70, 1), Vec3i(50, 160, 1), Vec3i(70, 80, 1), Vec3f(255.f, 0.f, 0.f), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 0.f, 255.f), zbuffer);
+		FillTriangle(&backbuffer, Vec3i(180, 50, 1), Vec3i(150, 1, 1), Vec3i(70, 180, 1), Vec3f(255.f, 255.f, 255.f), Vec3f(255.f, 255.f, 255.f), Vec3f(0.f, 0.f, 255.f), zbuffer);
+		FillTriangle(&backbuffer, Vec3i(180, 150, 1), Vec3i(120, 160, 1), Vec3i(130, 180, 1), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 255.f, 0.f), Vec3f(0.f, 255.f, 0.f), zbuffer);
 
 		vec3 light = Vec3f(0.f, 0.f, -1.f);
 
 		for (s32 i = 0; i < model->num_faces; i++) {
-			vec2 screen_coords[3];
+			vec3 screen_coords[3];
 			vec3 world_coords[3];
 			vec3 normal;
 			f32 intensity;
@@ -223,6 +240,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 				screen_coords[j].x = (s32)((vertex.x + 1) / 2 * backbuffer.width);
 				screen_coords[j].y = (s32)((vertex.y + 1) / 2 * backbuffer.height);
 				screen_coords[j].y = backbuffer.height - screen_coords[j].y; // flip image
+				screen_coords[j].z = (s32)((vertex.z + 0.5) * 128);
 
 				world_coords[j] = vertex;
 			}
@@ -237,7 +255,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 				colour.r = intensity * 255;
 				colour.g = intensity * 255;
 				colour.b = intensity * 255;
-				FillTriangle(&backbuffer, screen_coords[0], screen_coords[1], screen_coords[2], colour, colour, colour);
+				FillTriangle(&backbuffer, screen_coords[0], screen_coords[1], screen_coords[2], colour, colour, colour, zbuffer);
 			}
 		}
 		
